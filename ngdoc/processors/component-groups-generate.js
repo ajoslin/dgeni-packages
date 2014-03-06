b926715687ae4bbe9b8c43f911f141224385d381
Andy Joslin <andytjoslin@gmail.com>
2014-03-06 08:14:52 -0700

    config outputPathBase

--- ngdoc/processors/component-groups-generate.js
+++ ngdoc/processors/component-groups-generate.js
@@ -1,10 +1,14 @@
 var _ = require('lodash');
 
+var processorConfig;
 module.exports = {
   name: 'component-groups-generate',
   description: 'Add new component-groups docs',
   runAfter: ['adding-extra-docs'],
   runBefore: ['extra-docs-added'],
+  init: function(config) {
+    processorConfig = config.get('processing.component-groups-generate', {});
+  },
   process: function(docs, moduleMap) {
     _.forEach(moduleMap, function(module) {
 
@@ -23,7 +27,11 @@ module.exports = {
             moduleDoc: module,
             area: module.area,
             components: docs,
-            outputPath: _.template('partials/${module.area}/${module.name}/${docType}/index.html', { module: module, docType: docType }),
+            outputPath: _.template('${base}/${module.area}/${module.name}/${docType}/index.html', {
+              module: module, 
+              docType: docType ,
+              base: processorConfig.outputPathBase || 'partials'
+            }),
             path: _.template('${module.area}/${module.name}/${docType}', { module: module, docType: docType })
           };
         })
